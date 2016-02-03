cc0003a1ee4159c534bf0f3b7058dbd3159375b8
MichaelWang-6127 <mikewang.6127@gmail.com>
2016-02-03 09:13:04 -0600

    Update ClockWriter.java with a method 24hours
    gives option for 24 hour clock or 12 hour clock

--- ClockWriter.java
+++ ClockWriter.java
@@ -1,3 +1,11 @@
+//**********************************************************
+// Assignment: Clock Project1 ClockWriter.java file
+// Account: MichaelWang-6127
+//
+// Author: Michael Wang
+//
+// Date: 02/03/2016
+//*********************************************************
 public class ClockerWriter
 {
   public static void main(String[] args)
@@ -5,8 +13,20 @@ public class ClockerWriter
   int m_hour = 0;
   int m_minutes = 0;
   int m_seconds = 0;
+  int m_timeUpperBound = 0;
+  bool m_timeZone = 0;
   
   
-  
+  }
+  public static bool is24Hour(bool time)
+  {
+    if(time == true)
+    {
+      m_timeUpperBound = 24;
+    }
+    else()
+    {
+      m_timeUpperBound = 12;
+    }
   }
 }
