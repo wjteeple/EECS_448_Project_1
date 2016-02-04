12044215c2e35024cf1d606f7aa5be82a7ebf7f8
Austin Bailey <a604b997@ku.edu>
2016-02-04 13:49:21 -0600

    Add time triple class and method to facilitate clock initialization,

--- src/verision0/Main.java
+++ src/verision0/Main.java
@@ -15,6 +15,7 @@ public class Main
 //		boolean initialSeen=false;//When reading input from user, will determine if the user has seen the prompt for the first time or not
 		boolean exit = false;//determines if we should exit loop
 		int choice;
+		int min, hr, sec;
 		pln("Welcome to the clock!\nplease interact with the clock by pressing a number and hitting enter");
 		while(!exit)
 		{
@@ -36,6 +37,60 @@ public class Main
 				exit=false;
 			}
 		}
+	}
+	public static TimeTriple setTime()
+	{
+		boolean exit = false;
+		int sec=0;
+		int min=0;
+		int hr=0;
+		int choice;
+		while(!exit)
+		{
+			pln("At what second would you like to set the clock?");
+			choice = myScanner.nextInt();//TODO fix so it takes and converts string input
+			if(choice>=0&&choice<=59)
+			{
+				sec = choice;
+				exit = true;
+			}
+			else
+			{
+				pln("Please input a number betwn 0 and 59");
+			}
+		}
+		exit=false;
+		while(!exit)
+		{
+			pln("At what minute would you like to set the clock?");
+			choice = myScanner.nextInt();//TODO fix so takes and converts string input
+			if(choice>=0&&choice<=59)
+			{
+				min=choice;
+				exit=true;
+			}
+			else
+			{
+				pln("Please input a number between 0 and 59");
+			}
+		}
+		exit=false;
+		while(!exit)
+		{
+			pln("At what hr would you like to set the clock?");
+			choice = myScanner.nextInt();//TODO fix so takes and converts string input
+			if(choice>=0&&choice<=23)
+			{
+				hr=choice;
+				exit=true;
+			}
+			else
+			{
+				pln("Please input a between 0 and 23");
+			}
+		}
+		TimeTriple myTimeTriple = new TimeTriple(hr,min,sec);
+		return myTimeTriple;
 	}
 	public static void pln(String s)//time-saving method
 	{
---
+++ src/verision0/TimeTriple.java
@@ -0,0 +1,20 @@
+/**
+ *		@file:		TimeTriple.java
+ *		@author:	Austin Bailey
+ *		@date:		2/4/2016
+ *		@purpose:	To make it easy to pass Hours, minutes and seconds while programmi
+ */
+package verision0;
+
+public class TimeTriple 
+{
+	TimeTriple(int hr, int min, int sec)
+	{
+		m_hr = hr;
+		m_min = min;
+		m_sec = sec;
+	}
+	int m_hr;
+	int m_min;
+	int m_sec;
+}
