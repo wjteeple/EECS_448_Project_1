b2918c5e3838e79cf94a4c4fc0f522c2f6ea5208
MichaelWang-6127 <mikewang.6127@gmail.com>
2016-02-04 12:53:22 -0600

    Change ClockWriter.java to Clock.java
    Import several libraries for timing use.

--- ClockWriter.java
+++ ClockWriter.java
@@ -1,18 +1,24 @@
 //**********************************************************
-// Assignment: Clock Project1 ClockWriter.java file
+// Assignment: Clock Project1 Clock.java file
 // Account: MichaelWang-6127
 //
 // Author: Michael Wang
 //
 // Date: 02/03/2016
 //*********************************************************
-public class ClockerWriter
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.*;
+import java.lang.Object;
+public class Clock
 {
   int m_hour = 0;
   int m_minute = 0;
   int m_second = 0;
   int m_timeUpperBound = 0;
   bool m_timeZone = 0;
+  long timeNow = 0;
+  long timeLater = 0;
   
   public static void setTime(int hours, int minutes, int seconds)//sets the time with user input.
   {
@@ -40,6 +46,6 @@ public class ClockerWriter
   
   public static void calculateTime()
   {
-	  
+	  timeNow = System.currentTimeMillis();
   }
 }
